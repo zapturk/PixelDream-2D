676aacc85d20fa9350aaf4e7c9536d1cda7a46e0
Scott McKeefer <Scottzapturk19@gmail.com>
2024-11-21 20:44:09 -0600

    small tweeks

--- src/PixelDream2D/Models/AnimatedSprite.cs
+++ src/PixelDream2D/Models/AnimatedSprite.cs
@@ -9,81 +9,88 @@ namespace PixelDream2D.Models;
 public class AnimatedSprite
 {
     // Number of frames in the animation.
-    private int frameCount;
+    private int numOfFrames;
     
     // The animation spritesheet.
-    private Texture2D myTexture;
+    private Texture2D _myTexture;
     
     // The number of frames to draw per second.
-    private float timePerFrame;
+    private float _timePerFrame;
     
     // The current frame being drawn.
-    private int frame;
+    private int _currentFrame;
     
     // Total amount of time the animation has been running.
-    private float totalElapsed;
+    private float _totalElapsed;
     
     // Is the animation currently running?
-    private bool isPaused;
+    private bool _isPaused;
 
     // The current rotation, scale and draw depth for the animation.
-    public float Rotation, Scale, Depth;
-    
+    public readonly float Rotation;
+    public readonly float Scale;
+    public readonly float Depth;
+
     // The origin point of the animated texture.
     public Vector2 Origin;
 
     public AnimatedSprite(Vector2 origin, float rotation, float scale, float depth)
     {
-        this.Origin = origin;
-        this.Rotation = rotation;
-        this.Scale = scale;
-        this.Depth = depth;
+        Origin = origin;
+        Rotation = rotation;
+        Scale = scale;
+        Depth = depth;
     }
 
     public void Load(ContentManager content, string asset, int frameCount, int framesPerSec)
     {
-        this.frameCount = frameCount;
-        myTexture = content.Load<Texture2D>(asset);
-        timePerFrame = (float)1 / framesPerSec;
-        frame = 0;
-        totalElapsed = 0;
-        isPaused = false;
+        numOfFrames = frameCount;
+        _myTexture = content.Load<Texture2D>(asset);
+        _timePerFrame = 1.0f / framesPerSec;
+        _currentFrame = 0;
+        _totalElapsed = 0;
+        _isPaused = false;
     }
 
     public void UpdateFrame(float elapsed)
     {
-        if (isPaused)
+        // do nothing if it is paused
+        if (_isPaused)
             return;
-        totalElapsed += elapsed;
-        if (totalElapsed > timePerFrame)
+        
+        _totalElapsed += elapsed;
+        if (_totalElapsed > _timePerFrame)
         {
-            frame++;
+            _currentFrame++;
             // Keep the Frame between 0 and the total frames, minus one.
-            frame %= frameCount;
-            totalElapsed -= timePerFrame;
+            _currentFrame %= numOfFrames;
+            _totalElapsed -= _timePerFrame;
         }
     }
 
     public void DrawFrame(SpriteBatch batch, Vector2 screenPos)
     {
-        DrawFrame(batch, frame, screenPos);
+        DrawFrame(batch, _currentFrame, screenPos);
     }
 
-    public void DrawFrame(SpriteBatch batch, int frame, Vector2 screenPos)
+    public void DrawFrame(SpriteBatch batch, int frameToDraw, Vector2 screenPos)
     {
-        int frameWidth = myTexture.Width / frameCount;
-        Rectangle sourcerect = new Rectangle(frameWidth * frame, 0,
-            frameWidth, myTexture.Height);
-        batch.Draw(myTexture, screenPos, sourcerect, Color.White,
-            Rotation, Origin, Scale, SpriteEffects.None, Depth);
+        int frameWidth = _myTexture.Width / numOfFrames;
+        
+        Rectangle sourcerect = new Rectangle(frameWidth * frameToDraw, 0, frameWidth, _myTexture.Height);
+        
+        batch.Draw(_myTexture, screenPos, sourcerect, Color.White, Rotation, Origin, Scale, SpriteEffects.None, Depth);
     }
 
-    public bool IsPaused => isPaused;
+    public bool IsPlaying()
+    {
+        return !_isPaused;
+    } 
 
     public void Reset()
     {
-        frame = 0;
-        totalElapsed = 0f;
+        _currentFrame = 0;
+        _totalElapsed = 0f;
     }
 
     public void Stop()
@@ -94,11 +101,11 @@ public class AnimatedSprite
 
     public void Play()
     {
-        isPaused = false;
+        _isPaused = false;
     }
 
     public void Pause()
     {
-        isPaused = true;
+        _isPaused = true;
     }
 }
--- src/PixelDream2D/Models/Sprite.cs
+++ src/PixelDream2D/Models/Sprite.cs
@@ -5,11 +5,10 @@ namespace PixelDream2D.Models;
 
 public class Sprite(Texture2D texture, Vector2 position)
 {
-    private readonly Texture2D Texture = texture;
-    private readonly Vector2 Position = position;
+    private Texture2D Texture = texture;
 
     public virtual void Draw(SpriteBatch spriteBatch)
     {
-        spriteBatch.Draw(Texture, Position, Color.White);
+        spriteBatch.Draw(Texture, position, Color.White);
     }
 }
