a60c9f57758fa7777ebf952c4388e0c16c46389f
Scott McKeefer <Scottzapturk19@gmail.com>
2025-05-08 13:35:30 -0500

    Update Player.cs

--- src/PixelDream2D.Test/Entities/Player.cs
+++ src/PixelDream2D.Test/Entities/Player.cs
@@ -6,11 +6,34 @@ namespace PixelDream2D.Test.Entities;
 
 public class Player : Entity
 {
+    private Vector2 _position;
+    private bool _visible;
     private Sprite _sprite;
+    
     public Player(Vector2 position, bool visible = true) : base()
+{
+    _position = position;
+    _visible = visible;
+    
+}
+
+public void Move(int direction)
+{
+    switch (direction)
     {
-        
+        case 0: // Left
+            _position.X -= 5; // Adjust by a desired amount
+            break;
+        case 1: // Right
+            _position.X += 5; // Adjust by a desired amount
+            break;
+        case 2: // Up
+            _position.Y -= 5; // Adjust by a desired amount
+            break;
+        case 3: // Down
+            _position.Y += 5; // Adjust by a desired amount
+            break;
     }
-    
-    
+}
+
 }
